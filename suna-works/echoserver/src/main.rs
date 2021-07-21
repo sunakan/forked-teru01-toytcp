8c00337409e0c12ddd704e3b0b89259b0e429b65
sunakan <sunakan03@gmail.com>
2021-07-21 23:28:00 +0900

    コメントを打った

--- suna-works/echoserver/src/main.rs
+++ suna-works/echoserver/src/main.rs
@@ -12,19 +12,20 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn echo_server(address: &str) -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind(address)?;
+    let listener = TcpListener::bind(address)?; // [1] ソケットの作成
     loop {
-        let (mut stream, _) = listener.accept()?;
+        let (mut stream, _) = listener.accept()?; // [2] スレッドをブロックし、クライアントからの確立要求を待機
         // スレッドを立ち上げて接続に対処する
-        thread::spawn(move || {
+        thread::spawn(move || { // [3] 新しいスレッドを生む, moveとあるのでstreamが新しいスレッドへmove
             let mut buffer = [0u8; 1024];
             loop {
-                let nbytes = stream.read(&mut buffer).unwrap();
-                if nbytes == 0 {
+                let nbytes = stream.read(&mut buffer).unwrap(); // [4] スレッドをブロック、データの受信を待機
+                println!("{:?}", buffer);
+                if nbytes == 0 { // [6] Ctrl+cとか
                     return;
                 }
                 print!("{}", str::from_utf8(&buffer[..nbytes]).unwrap());
-                stream.write_all(&buffer[..nbytes]).unwrap();
+                stream.write_all(&buffer[..nbytes]).unwrap(); // [5] クライアントに対して書き込む
             }
         });
     }
